525ea25f4d88d9377f01df4a0177a2b9995ff33a
Prateek Bhatnagar <prateek89born@gmail.com>
2019-09-06 15:23:24 -0700

    adding netlify plugin

--- preact.config.js
+++ preact.config.js
@@ -1,9 +1,11 @@
 const { PreactCliMDCrawlerPlugin } = require('./src/crawler/plugin');
+const netlifyPlugin = require('preact-cli-plugin-netlify');
 const { join } = require('path');
 
 module.exports = (config) => {
 	config.plugins.push(new PreactCliMDCrawlerPlugin({
 		src: join(__dirname, 'content')
 	}));
+	netlifyPlugin(config);
 	return config;
 };
