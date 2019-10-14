03d767a61e1a2f33df5dd07fd509d83e328527ba
Prateek Bhatnagar <prateek89born@gmail.com>
2019-10-14 16:59:44 -0700

    responsive fixes

--- src/routes/blogs/index.js
+++ src/routes/blogs/index.js
@@ -5,34 +5,47 @@ import style from './style';
 
 const blogs = (props) => {
 	const [data, isLoading] = usePrerenderData(props);
+	return (
+		<div class={style.pageBlogs}>
+			<h1 class={style.pageTitle}>My Blogs</h1>
+			{ getBlogsListing(data, isLoading) }
+		</div>
+	);
+};
+
+function getBlogsListing(data, isLoading) {
 	if (isLoading) {
-		return (<h1>Loading</h1>);
+		return (
+			<article class={style.loadingPlaceholder}>
+				<h2 class={`${style.blogtitle} loading`}>&nbsp;</h2>
+				<div class={`${style.loadingBody} loading`}>&nbsp;</div>
+				<div class={`${style.loadingBody} loading`}>&nbsp;</div>
+				<div class={`${style.loadingBody} loading`}>&nbsp;</div>
+			</article>
+		);
 	}
 	if (data && data.data) {
 		const { data: blogs } = data;
 		return (
-			<div class={style.pageBlogs}>
-				<h1 class={style.pageTitle}>My Blogs</h1>
+			<>
 				{blogs.edges.map(blog => (
-					<Link href={`/blog/${blog.id}`}>
-						<article>
-							<h2>{blog.details.title}</h2>
-							<div>
-								{
-									(blog.details.tags.substr(1, blog.details.tags.length - 2).split(',') || []).map(tag => <span class={style.tag}>{tag}</span>)
-								}
-							</div>
-							<p class={style.preview}>
-								{blog.preview}
-							</p>
-						</article>
-					</Link>
-				))}
-			</div>
+				<Link href={`/blog/${blog.id}`}>
+					<article>
+						<h2>{blog.details.title}</h2>
+						<div>
+							{
+								(blog.details.tags.substr(1, blog.details.tags.length - 2).split(',') || []).map(tag => <span class={style.tag}>{tag}</span>)
+							}
+						</div>
+						<p class={style.preview}>
+							{blog.preview}
+						</p>
+					</article>
+				</Link>
+			))}
+			</>
 		);
 	}
-
-	return (<h1>Error</h1>);
-};
+}
 
 export default blogs;
